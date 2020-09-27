856339013f692111359f063e7367bf916e3bb177
Col. Schwi <VCCNStardust@gmail.com>
2020-09-27 19:41:25 +0800

    更改了某些机制，现在只需要一个txt了；readme稍后更新（晚自习不太方便）

--- ISSServerProt/Program.cs
+++ ISSServerProt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,21 +15,20 @@ namespace IssServerProt
         private static bool ErrorOccured;
         public static void Main(string[] args)
         {
-            string startArgs = default;
             ushort rconPort = default;
             string serverName = default;
             string rconPassword = default;
-            string Command= default;
+            string AllConfig = File.ReadAllText("Config.txt");
+            var ConfigE = AllConfig.Split(Environment.NewLine);
             var parser = new FluentCommandLineParser();
-            parser.Setup<string>("argsFile").Required().Callback(it => startArgs = File.ReadAllText(it));
             parser.Setup<int>("rconPort").Required().Callback(it => rconPort = (ushort)it);
             parser.Setup<string>("name").Callback(it => serverName = it);
             parser.Setup<string>("rconPassword").Required().Callback(it => rconPassword = it);
-            parser.Setup<string>("CommandFile").Required().Callback(it => Command = File.ReadAllText(it));
             parser.Parse(args);
 
-            var CommandE = Command.Split(Environment.NewLine);
-            var CmdLength = CommandE.Length;
+            string startArgs = ConfigE.First;
+
+            var CmdLength = ConfigE.Length -1;
 
             Console.Title = $"ISS {serverName} Server";
 
@@ -81,7 +81,7 @@ namespace IssServerProt
                         int n = 1;
                         while (n <= CmdLength)
                         {
-                            rcon.SendCommandAsync(CommandE.GetValue(n-1).ToString());
+                            rcon.SendCommandAsync(ConfigE.GetValue(n).ToString());
                             n++;
                         }
                         Console.WriteLine($"{serverName} Server Modified");
